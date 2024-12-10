e0d130c4f7630b32a985b469bb64d3cb4ef69e34
Sunanda01 <sunandasadhukhan1@gmail.com>
2024-12-11 00:15:30 +0530

    Update

--- Controller/DocumentController.js
+++ Controller/DocumentController.js
@@ -12,7 +12,7 @@ const docController= {
    try{
       const document_Model=await documentmodels.create(fileObj);
       // console.log(document_Model);
-      return res.status(200).json({path:`${url}${document_Model._id}`});
+      return res.status(200).json({path:`${url}/document_Model/${document_Model._id}`});
    }catch(err){
       console.error(err.message);
       return res.status(500).json({error:err.message});
@@ -21,11 +21,14 @@ const docController= {
 async fileDownload(req,res){
    try{
       const file= await documentmodels.findById(req.params.id);
+      if (!file) {
+         return res.status(404).json({ error: 'File not found' });
+       }    
       file.downloadContent++;
       await file.save();
       res.download(file.path,file.name);
    }catch(err){
-      console.error(err.message);
+      res.status(500).json({ error: err.message });
    }
 }
 }
