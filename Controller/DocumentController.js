1a8a55d9488eef0909aab8bcb37e7f998c4c7a89
Sunanda Sadhukhan <sunandasadhukhan1@gmail.com>
2024-12-11 00:29:36 +0530

    Update DocumentController.js

--- Controller/DocumentController.js
+++ Controller/DocumentController.js
@@ -12,7 +12,7 @@ const docController= {
    try{
       const document_Model=await documentmodels.create(fileObj);
       // console.log(document_Model);
-      return res.status(200).json({path:`${url}document_Model${document_Model._id}`});
+      return res.status(200).json({path:`${url}/document_Model/${document_Model._id}`});
    }catch(err){
       console.error(err.message);
       return res.status(500).json({error:err.messag`e});
